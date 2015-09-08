d92d6510ba1c302bd53a1ae8053c88ad5e65d4de
comleaf <shether@live.com>
2015-09-08 08:06:34 +0800

    123
    12

---
+++ SwiftHTTP.h
@@ -0,0 +1,13 @@
+//
+//  SwiftHTTP.h
+//  SwiftHTTP
+//
+//  Created by Comleaf on 15/8/11.
+//  Copyright (c) 2015年 Comleaf. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SwiftHTTP : NSObject
+
+@end
